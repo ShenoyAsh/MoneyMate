16174249e085eff3b57d1e2f257443dbb71e602c
ShenoyAsh <shenoybashwini@gmail.com>
2025-11-26 05:46:06 +0530

    fixing ai features

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images:{
-    remotePatterns:[{
-      protocol:'https',
-      hostname:"randomuser.me",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
     },
-  ],
   },
 };
 
